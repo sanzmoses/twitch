82f66fe9ce2a93bd449ce3a561952e795a397335
sanzillion <sanzimagery@gmail.com>
2017-05-10 16:00:54 +0800

    add designs

--- masterjs.js
+++ masterjs.js
@@ -11,8 +11,23 @@ $(document).ready(function(){
             console.log(data);
         }});
 
+   var box = $('.bg-green');
+   box.css("height", "0px"); //set property
+   box.animate({height: "420px"}, 1000);
+
+   $('li').on("click", function(){
+        var id = $(this).attr('id');
+        var element = $('#'+id);
+        if(id){
+            element.addClass('focus');
+            element.siblings().removeClass();
+        }
+
+
+   });
+
 });
 
 var test = "http://www.zacharydurland.com/twitch-app/";
 var link = "https://wind-bow.glitch.me/twitch-api/channels/ogamingsc2?callback=?";
-https://wind-bow.glitch.me/twitch-api/streams/passwordgamer?callback=?
+    
