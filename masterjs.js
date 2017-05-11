dbc3383773ff8b762329f337c914ea6e755fa64c
sanzillion <sanzimagery@gmail.com>
2017-05-11 21:22:21 +0800

    Done 99%

--- masterjs.js
+++ masterjs.js
@@ -7,6 +7,9 @@ var arr = [];
 var online = [];
 var offline = [];
 var basura = [];
+var tb = $('.tb');
+var load = $('.trans');
+var track = '';
 // for(var x = 0; x < 5; x++){
 //     var channels = {};
 //     channels.logo = 'Logo no. '+ (x + 1);
@@ -22,6 +25,7 @@ var channel = ["OgamingSC2", "freecodecamp", "cretetion", "p4wnyhof", "comster40
 var channels = ["OgamingSC2", "freecodecamp","comster404"];
 
    var box = $('.bg-green');
+   tb.css("opacity", "0");
    //box.css("height", "0px"); //set property
    //box.animate({height: "420px"}, 1000);
 
@@ -130,20 +134,83 @@ counter = channel.length;
     }
 
     var output = function(){
-                $(".tb").empty();
+        var rm = function(){
+            load.remove();
+            tb.animate({opacity: "1"});
+        };
+        load.animate({opacity: "0"}, rm);
+        tb.empty();
         for(var y = 0; y < online.length; y++){
-            $(".tb").append('<tr class="online"><td width="20%"><img src="'+online[y].logo+'" class="img"></td><td width="60%"><a href="'+online[y].url+'" target="_blank"><b>'+online[y].names+'</b></a><a href="'+online[y].url+'" target="_blank">Streaming: '+online[y].stream+'</a></td><td width="20%"><i class="fa fa-check"></i><a href="#"></a></td></tr>');
+            tb.append('<tr class="online"><td width="20%"><img src="'+online[y].logo+'" class="img"></td><td width="70%" id="td"><a href="'+online[y].url+'" target="_blank"><b>'+online[y].names+'</b></a><br><a href="'+online[y].url+'" target="_blank">Streaming: '+online[y].stream+'</a></td><td width="10%"><i class="fa fa-check"></i><a href="#"></a></td></tr><hr>');
         }
 
         for(var y = 0; y < offline.length; y++){
-            $(".tb").append('<tr class="offline"><td width="20%"><img src="'+offline[y].logo+'" class="img"></td><td width="60%"><a href="'+offline[y].url+'" target="_blank"><b>'+offline[y].names+'</b></a><p>Not Streaming</p></td><td width="20%"><i class="fa fa-close"></i><a href="#"></a></td></tr>');
+            tb.append('<tr class="offline"><td width="20%"><img src="'+offline[y].logo+'" class="img"></td><td width="70%" id="td"><a href="'+offline[y].url+'" target="_blank"><b>'+offline[y].names+'</b></a><br><p>Not Streaming</p></td><td width="10%"><i class="fa fa-close"></i><a href="#"></a></td></tr><hr>');
         }
 
         for(var y = 0; y < basura.length; y++){
-            $(".tb").append('<tr class="nada"><td width="20%"><img src="x.jpg" class="img"></td><td width="60%"><p><b>'+basura[y].names+'</b></p><p>Account dont exist!</p></td><td width="20%"><i class="fa fa-exclamation-circle"></i><a href="#"></a></td></tr>');
+            tb.append('<tr class="nada"><td width="20%"><img src="x.jpg" class="img"></td><td width="70%" id="td"><p><b>'+basura[y].names+'</b></p><p>Account dont exist!</p></td><td width="10%"><i class="fa fa-exclamation-circle"></i><a href="#"></a></td></tr><hr>');
         }
     }
 
+    var output1 = function(){
+        console.log("online output");
+        tb.animate({opacity: "1"});
+        for(var y = 0; y < online.length; y++){
+                tb.append('<tr class="online"><td width="20%"><img src="'+online[y].logo+'" class="img"></td><td width="70%" id="td"><a href="'+online[y].url+'" target="_blank"><b>'+online[y].names+'</b></a><br><a href="'+online[y].url+'" target="_blank">Streaming: '+online[y].stream+'</a></td><td width="10%"><i class="fa fa-check"></i><a href="#"></a></td></tr><hr>');
+        }
+    }
+
+    var output2 = function(){
+        console.log("offline output");
+        tb.animate({opacity: "1"});
+        for(var y = 0; y < offline.length; y++){
+                tb.append('<tr class="offline"><td width="20%"><img src="'+offline[y].logo+'" class="img"></td><td width="70%" id="td"><a href="'+offline[y].url+'" target="_blank"><b>'+offline[y].names+'</b></a><br><p>Not Streaming</p></td><td width="10%"><i class="fa fa-close"></i><a href="#"></a></td></tr><hr>');
+        }
+    }
+
+    $('#ol').on('click', function(){
+        if(track == "ol"){
+            // do nothing
+        }
+        else{
+          track = "ol";
+            console.log("online");
+            tb.animate({opacity: "0"}, 100, function(){
+                console.log("Trying to empty");
+                tb.empty();
+                output1();
+            });  
+        }        
+    });
+
+    $('#of').on('click', function(){
+        if(track == "of"){
+            //do nothing
+        }
+        else{
+            track = "of";
+            console.log("offline");
+            tb.animate({opacity: "0"}, 100, function(){
+                console.log("trying to empty");
+                tb.empty();
+                output2();
+            });
+        }
+    });
+
+    $('#al').on('click', function(){
+        if(track == "al"){
+            //do nothing
+        }
+        else{
+           track = "al";
+            tb.animate({opacity: "0"}, 100, function(){
+                tb.empty();
+                output();
+            }); 
+        }
+    });
 
 
 });
